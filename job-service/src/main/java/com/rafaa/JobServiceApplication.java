8bf861e0ae407e5be025f201bf00cbeffeb688f0
rafaa1999 <raferhouma10@gmail.com>
2024-05-05 10:20:54 +0100

    Set up project

---
+++ job-service/src/main/java/com/rafaa/JobServiceApplication.java
@@ -0,0 +1,10 @@
+package com.rafaa;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobServiceApplication {
+    public static void main(String[] args) {
+
+    }
+}
