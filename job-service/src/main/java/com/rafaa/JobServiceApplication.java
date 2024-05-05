3d075bb390aba3d9568641a3dec3ec37e10f76f0
rafaa1999 <raferhouma10@gmail.com>
2024-05-05 10:51:32 +0100

    Manage main job functionalities

--- job-service/src/main/java/com/rafaa/JobServiceApplication.java
+++ job-service/src/main/java/com/rafaa/JobServiceApplication.java
@@ -5,6 +5,5 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JobServiceApplication {
     public static void main(String[] args) {
-
     }
 }
